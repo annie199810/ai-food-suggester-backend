753c9d3470e793027f52be1c504cbe6cc3375c28
Annie <developerannie057@gmail.com>
2026-07-02 22:22:28 +0530

    Added root route

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "AI Food Suggester Backend is running 🚀",
+  });
+});
+
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/suggest", require("./routes/suggest"));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT} ✅`);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log(err));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} ✅`);
 });
